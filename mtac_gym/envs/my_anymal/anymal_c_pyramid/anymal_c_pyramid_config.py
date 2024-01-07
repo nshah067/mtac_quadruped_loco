a13ea0446b1a033b44ff92a4e3777ba7a5585e9c
nshah067 <60299940+nshah067@users.noreply.github.com>
2024-01-06 19:34:13 -0500

    Added proper cfg files

---
+++ mtac_gym/envs/my_anymal/anymal_c_pyramid/anymal_c_pyramid_config.py
@@ -0,0 +1,106 @@
+# SPDX-FileCopyrightText: Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Copyright (c) 2021 ETH Zurich, Nikita Rudin
+
+from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
+
+class AnymalCPyraCfg( LeggedRobotCfg ):
+    class env( LeggedRobotCfg.env ):
+        num_envs = 4096
+        num_actions = 12
+
+    class terrain( LeggedRobotCfg.terrain ):
+        mesh_type = 'trimesh'
+        horizontal_scale = 0.1 # [m]
+        vertical_scale = 0.005 # [m]
+        # terrain types: [smooth slope, rough slope, stairs up, stairs down, discrete]
+        #terrain_proportions = [0, 0, 1, 0, 0]
+        curriculum = False
+        selected = True
+        num_rows= 10 # number of terrain rows (levels)
+        num_cols = 10 # number of terrain cols 
+        terrain_kwargs = {
+            'type' : 'pyramid2'
+        }
+
+    class init_state( LeggedRobotCfg.init_state ):
+        pos = [0.0, 0.0, 0.6] # x,y,z [m]
+        default_joint_angles = { # = target angles [rad] when action = 0.0
+            "LF_HAA": 0.0,
+            "LH_HAA": 0.0,
+            "RF_HAA": -0.0,
+            "RH_HAA": -0.0,
+
+            "LF_HFE": 0.4,
+            "LH_HFE": -0.4,
+            "RF_HFE": 0.4,
+            "RH_HFE": -0.4,
+
+            "LF_KFE": -0.8,
+            "LH_KFE": 0.8,
+            "RF_KFE": -0.8,
+            "RH_KFE": 0.8,
+        }
+
+    class control( LeggedRobotCfg.control ):
+        # PD Drive parameters:
+        stiffness = {'HAA': 80., 'HFE': 80., 'KFE': 80.}  # [N*m/rad]
+        damping = {'HAA': 2., 'HFE': 2., 'KFE': 2.}     # [N*m*s/rad]
+        # action scale: target angle = actionScale * action + defaultAngle
+        action_scale = 0.5
+        # decimation: Number of control action updates @ sim DT per policy DT
+        decimation = 4
+        use_actuator_network = True
+        actuator_net_file = "{LEGGED_GYM_ROOT_DIR}/resources/actuator_nets/anydrive_v3_lstm.pt"
+
+    class asset( LeggedRobotCfg.asset ):
+        file = "{LEGGED_GYM_ROOT_DIR}/resources/robots/anymal_c/urdf/anymal_c.urdf"
+        name = "anymal_c"
+        foot_name = "FOOT"
+        penalize_contacts_on = ["SHANK", "THIGH"]
+        terminate_after_contacts_on = ["base"]
+        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
+
+    class domain_rand( LeggedRobotCfg.domain_rand):
+        randomize_base_mass = True
+        added_mass_range = [-5., 5.]
+  
+    class rewards( LeggedRobotCfg.rewards ):
+        base_height_target = 0.5
+        max_contact_force = 500.
+        only_positive_rewards = True
+        class scales( LeggedRobotCfg.rewards.scales ):
+            pass
+
+class AnymalCPyraCfgPPO( LeggedRobotCfgPPO ):
+    class runner( LeggedRobotCfgPPO.runner ):
+        run_name = 'PyramidRun'
+        experiment_name = 'stair_pyramid_anymal_c'
+        resume = False
+        load_run = -1
